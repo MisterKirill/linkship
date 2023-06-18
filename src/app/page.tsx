8f92d2da712138c6405aa858f6a95180559ed630
Kirill <misterkirill666@gmail.com>
2023-06-18 15:21:17 +0500

    NextJS, Prisma initialization

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+	title: 'LoungeTalks - Home',
+	description: 'LoungeTalks is a new social network'
+}
+
+export default function Home() {
+	return (
+		<>
+			<span>Hello world</span>
+		</>
+	)
+}
